17a7e97ffba95d07c808aeb738e4887e324d5731
M. Mudassir Awan <121383917+Mudasssirr@users.noreply.github.com>
2023-03-21 13:41:43 +0500

    added JS A7 Ch26-30
    added JavaScript Assignment 7 from Chapter 26 to 30

---
+++ JavaScript A7 Ch26-30/app.js
@@ -0,0 +1,61 @@
+//Chapter 26-30
+
+//Question 1
+// var userInput = prompt('Enter positive integer');
+// var roundoff = Math.round(userInput);
+// var floorValue =  Math.floor(userInput);
+// var cielValue = Math.ceil(userInput);
+// document.write('number: ' + userInput + '<br />');
+// document.write('round off value: ' + roundoff + '<br />');
+// document.write('floor value: ' + floorValue + '<br />');
+// document.write('ciel value: ' + cielValue + '<br />');
+
+//Question 2
+// var userInput2 = prompt('Enter negative integer');
+// var roundoff2 = Math.round(userInput2);
+// var floorValue2 =  Math.floor(userInput2);
+// var cielValue2 = Math.ceil(userInput2);
+// document.write('number: ' + userInput2 + '<br />');
+// document.write('round off value: ' + roundoff2 + '<br />');
+// document.write('floor value: ' + floorValue2 + '<br />');
+// document.write('ciel value: ' + cielValue2 + '<br />');
+
+//Question 3
+// var userInput3 = prompt('Enter your number');
+// var absltValue = Math.abs(userInput3);
+// document.write('The absolute value of ' + userInput3 + ' is ' + absltValue);
+
+//Question 4
+// var dice = Math.random() * 6;
+// var round = Math.round(dice);
+// document.write('random dice value: ' + round);
+
+//Question 5
+// var toss = Math.random();
+// var round2 = Math.round(toss);
+// if(round2 === 1){
+//     document.write(round2 + '<br />')
+//     document.write('random coin value: Heads');
+// }else{
+//     document.write(round2 + '<br />')
+//     document.write('random coin value: Tails');
+// }
+
+//Question 6
+// var random2 = Math.random() * 100;
+// var round3 = Math.round(random2);
+// document.write('random number between 1 and 100: ' + round3);
+
+//Question 7
+// var userInput4 = prompt('Enter your weight is kilograms');
+// var parse = parseInt(userInput4);
+// document.write('The weight of the user is: ' + parse + ' kilograms');
+
+//Question 8
+// var randomNo = '8';
+// var userInput5 = prompt('Enter a number between 1-10');
+// if(userInput5 === randomNo){
+//     alert('Congratulations!');
+// }else{
+//     alert('Try Again');
+// }
